b23d6565a4602738fc58383fe437e804e54807cb
s-ALEAVITT <s-ALEAVITT@484E293863.lwsd.k12wa.org>
2022-05-13 13:52:23 -0700

    plotted all of the functions

---
+++ FuncPlotter/src/Function.java
@@ -0,0 +1,4 @@
+
+public interface Function {
+	int calc(int x);
+}
--- FuncPlotter/src/Program.java
+++ FuncPlotter/src/Program.java
@@ -13,12 +13,48 @@ public class Program {
         canvas = new Canvas();
         canvas.setRange(-360, -240, 360, 240);
         canvas.open();
+        Function[] functions = new Function[5];
+        functions[0] = (x) -> {return x;};
+        functions[1] = (x) -> {return (int) (200 * Math.sin(Math.PI * x / 360));};
+        functions[2] = (x) -> {return ((x - 200) * (x + 200)) / 250;};
+        functions[3] = (x) -> {return (int) (20 * Math.log(x));};
+        functions[4] = (x) -> {return (int) ((Math.pow(x, 3)) / 200000);};
 
         // Draw a short red diagonal on the canvas
         canvas.pause();
         canvas.setColor(Color.red);
-        for (int i = 0; i < 100; i++) {
-            canvas.plot(i, i);
+        
+        for (int x = 0; x < 100; x++) {
+            canvas.plot(x, functions[0].calc(x));
+        }
+        
+        // draw a sine wave in blue
+        canvas.pause();
+        canvas.setColor(Color.blue);
+        
+        for (int x = -360; x < 360; x++) {
+            canvas.plot(x, functions[1].calc(x));
+        }
+        
+        // draw a quadratic in green
+        canvas.pause();
+        canvas.setColor(Color.green);
+        for (int x = -360; x < 360; x++) {
+            canvas.plot(x, functions[2].calc(x));
+        }
+        
+        // draw a log graph in black
+        canvas.pause();
+        canvas.setColor(Color.black);
+        for (int x = -360; x < 360; x++) {
+            canvas.plot(x, functions[3].calc(x));
+        }
+        
+        // draw a cubic in orange
+        canvas.pause();
+        canvas.setColor(Color.orange);
+        for (int x = -360; x < 360; x++) {
+            canvas.plot(x, functions[4].calc(x));
         }
 
         // Pause and close the canvas then terminate the program.
